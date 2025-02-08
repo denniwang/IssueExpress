e780c9e52c1eeaa45d7b4e38a9a2f0e5da206b26
Xiaole Su <suxiaole0223@gmail.com>
2025-02-08 16:40:06 -0500

    applied css color

--- app/components/RoadFooter.tsx
+++ app/components/RoadFooter.tsx
@@ -1,7 +1,7 @@
 export default function RoadFooter() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-[#414141] py-16">
-      <div className="border-t-4 border-dashed border-white"></div>
+    <div className="fixed bottom-0 left-0 right-0 bg-[#0F2E4A] py-16">
+      <div className="border-t-4 border-dashed border-[#F7E3E1]"></div>
     </div>
   );
 }
