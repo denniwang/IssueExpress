fbd0215554af795b07155bb4e118858f5dec5813
russell-leung <russell-leung@comcast.net>
2025-02-08 15:35:56 -0500

    login screen

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,15 +1,21 @@
 import { signInAction } from "@/app/actions";
-import { Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 
 export default async function Login() {
   return (
-    <div id="login-body" className="w-screen h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('/login-background.png')" }}>
-      <form>
-        <SubmitButton pendingText="Signing In..." formAction={signInAction} className="bg-white text-black hover:bg-gray-400 hover:text-black">
-          Sign in to GitHub
-        </SubmitButton>
-      </form>
+    <div
+      id="login-body"
+      className="w-screen h-screen flex items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/login-background.png')" }}
+    >
+      <div className="flex flex-col justify-between" style={{ height: "95%"}}>
+        <h1 className="text-white text-3xl">Project Name</h1>
+        <form>
+          <SubmitButton pendingText="Signing In..." formAction={signInAction}>
+            Sign in to GitHub
+          </SubmitButton>
+        </form>
+      </div>
     </div>
   );
 }
