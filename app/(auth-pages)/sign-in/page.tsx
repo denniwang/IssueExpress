c1392b8960a3834a72e86d9c3ecbf2d2b93669f6
russell-leung <russell-leung@comcast.net>
2025-02-09 00:32:35 -0500

    change title font

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,5 +1,6 @@
 import { signInAction } from "@/app/actions";
 import { SubmitButton } from "@/components/submit-button";
+import "@fontsource/silkscreen";
 
 export default async function Login() {
   return (
@@ -8,8 +9,8 @@ export default async function Login() {
       className="w-screen h-screen flex items-center justify-center bg-cover bg-center font-retro"
       style={{ backgroundImage: "url('/login-background.png')" }}
     >
-      <div className="flex flex-col justify-between text-lg" style={{ height: "85%"}}>
-        <h1 className="mt-11 text-white text-9xl">IssueExpress</h1>
+      <div className="flex flex-col justify-between text-lg" style={{ height: "90%"}}>
+        <h1 className="mt-11 text-white text-8xl [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]" style={{ fontFamily: "Silkscreen, sans-serif" }}>IssueExpress</h1>
         <form className="flex flex-row justify-center">
           <SubmitButton pendingText="Signing In..." formAction={signInAction}>
             Sign in to GitHub
