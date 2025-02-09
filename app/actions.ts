2b797c728a78a6ae3d64fe30a3c7463e8f296df7
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2025-02-08 20:03:08 -0500

    submit button

--- app/actions.ts
+++ app/actions.ts
@@ -4,40 +4,8 @@ import { encodedRedirect } from "@/utils/utils";
 import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-
-export const signUpAction = async (formData: FormData) => {
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
-  const supabase = await createClient();
-  const origin = (await headers()).get("origin");
-
-  if (!email || !password) {
-    return encodedRedirect(
-      "error",
-      "/sign-up",
-      "Email and password are required"
-    );
-  }
-
-  const { error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      emailRedirectTo: `${origin}/auth/callback`,
-    },
-  });
-
-  if (error) {
-    console.error(error.code + " " + error.message);
-    return encodedRedirect("error", "/sign-up", error.message);
-  } else {
-    return encodedRedirect(
-      "success",
-      "/sign-up",
-      "Thanks for signing up! Please check your email for a verification link."
-    );
-  }
-};
+import Ticket from "./components/Ticket";
+import { Project } from "./components/project-selector";
 
 async function signInWithGithub() {
   const supabase = await createClient();
@@ -59,79 +27,46 @@ export const signInAction = async () => {
   }
 };
 
-export const forgotPasswordAction = async (formData: FormData) => {
-  const email = formData.get("email")?.toString();
+export const handleSubmit = async (
+  approvedTickets: Ticket[],
+  selectedProject: Project
+) => {
   const supabase = await createClient();
-  const origin = (await headers()).get("origin");
-  const callbackUrl = formData.get("callbackUrl")?.toString();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  if (!email) {
-    return encodedRedirect("error", "/forgot-password", "Email is required");
+  if (!user) {
+    return redirect("/sign-in");
   }
+  const { data, error } = await supabase.auth.getSession();
+  const accessToken = data.session?.provider_token;
 
-  const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: `${origin}/auth/callback?redirect_to=/protected/reset-password`,
+  approvedTickets.forEach(async (ticket) => {
+    try {
+      const response = await fetch("http://localhost:3000/api/createTicket", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          githubToken: accessToken,
+          owner: user.user_metadata.user_name,
+          repoName: selectedProject.name,
+          title: ticket.name,
+          description: ticket.description,
+          labelName: ticket.label,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create ticket");
+      }
+
+      const result = await response.json();
+      console.log("Created ticket:", result);
+    } catch (error) {
+      console.error("Error creating ticket:", error);
+    }
   });
-
-  if (error) {
-    console.error(error.message);
-    return encodedRedirect(
-      "error",
-      "/forgot-password",
-      "Could not reset password"
-    );
-  }
-
-  if (callbackUrl) {
-    return redirect(callbackUrl);
-  }
-
-  return encodedRedirect(
-    "success",
-    "/forgot-password",
-    "Check your email for a link to reset your password."
-  );
-};
-
-export const resetPasswordAction = async (formData: FormData) => {
-  const supabase = await createClient();
-
-  const password = formData.get("password") as string;
-  const confirmPassword = formData.get("confirmPassword") as string;
-
-  if (!password || !confirmPassword) {
-    encodedRedirect(
-      "error",
-      "/protected/reset-password",
-      "Password and confirm password are required"
-    );
-  }
-
-  if (password !== confirmPassword) {
-    encodedRedirect(
-      "error",
-      "/protected/reset-password",
-      "Passwords do not match"
-    );
-  }
-
-  const { error } = await supabase.auth.updateUser({
-    password: password,
-  });
-
-  if (error) {
-    encodedRedirect(
-      "error",
-      "/protected/reset-password",
-      "Password update failed"
-    );
-  }
-
-  encodedRedirect("success", "/protected/reset-password", "Password updated");
-};
-
-export const signOutAction = async () => {
-  const supabase = await createClient();
-  await supabase.auth.signOut();
-  return redirect("/sign-in");
 };
