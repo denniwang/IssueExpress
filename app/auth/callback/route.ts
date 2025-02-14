f3a2af5c5bb2a39f29aaa46b0b254a3fd851b0a9
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2025-02-13 23:31:54 -0500

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "/protected/repositories";
+  const next = "/protected/repositories";
 
   if (code) {
     const supabase = await createClient();
